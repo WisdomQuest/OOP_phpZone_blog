d76aa784eed46204577543f7a8065dd542479546
WisdomQuest <sofronovpv89@mail.ru>
2023-09-26 18:40:07 +0400

    www delete!!!

--- templates/main/main.php
+++ templates/main/main.php
@@ -13,30 +13,41 @@
 
 
 <div style="text-align: center">
-    <?php for ($pageNum = 1; $pageNum <= $pagesCount; $pageNum++): ?>
-        <?php if ($currentPageNum === $pageNum): ?>
-            <b><?= $pageNum ?></b>
+    <?php if ($currentPageNum !== 1): ?>
+        <a href="/<?= $str = 1 ?>">1 </a>
+        <span>---</span>
+    <?php endif; ?>
+
+    <?php for ($str = $currentPageNum; $str <= $currentPageNum + 5; $str++): ?>
+        <?php if ($currentPageNum === $str): ?>
+            <b><?= $str ?></b>
         <?php else: ?>
-            <a href="/<?= $pageNum === 1 ? '' : $pageNum ?>"><?= $pageNum ?></a>
+            <?php if ($str < $pagesCount): ?>
+                <a href="/<?= $str === 1 ? '' : $str ?>"><?= $str ?></a>
+            <?php endif; ?>
         <?php endif; ?>
     <?php endfor; ?>
+    <span>---</span>
+    <?php if ($currentPageNum != $pagesCount): ?>
+        <a href="/<?= $pagesCount ?>"><?= $pagesCount ?></a>
+    <?php endif; ?>
 </div>
 
-    <div style="text-align: center">
-        <?php if ($previousPageLink !== null): ?>
-            <a href="<?= $previousPageLink ?>">&lt; Туда</a>
-        <?php else: ?>
-            <span style="color: cadetblue">&lt; Туда</span>
-        <?php endif; ?>
-        &nbsp;&nbsp;&nbsp;
-        <?php if ($nextPageLink !== null): ?>
-            <a href="<?= $nextPageLink ?>">Сюда &gt;</a>
-        <?php else: ?>
-            <span style="color: darkgray">Сюда &gt;</span>
-        <?php endif; ?>
-    </div>
+<div style="text-align: center">
+    <?php if ($previousPageLink !== null): ?>
+        <a href="<?= $previousPageLink ?>">&lt; Туда</a>
+    <?php else: ?>
+        <span style="color: cadetblue">&lt; Туда</span>
+    <?php endif; ?>
+    &nbsp;&nbsp;&nbsp;
+    <?php if ($nextPageLink !== null): ?>
+        <a href="<?= $nextPageLink ?>">Сюда &gt;</a>
+    <?php else: ?>
+        <span style="color: darkgray">Сюда &gt;</span>
+    <?php endif; ?>
+</div>
 
 
-    <?php include __DIR__ . '/../footer.php'; ?>
+<?php include __DIR__ . '/../footer.php'; ?>
 
 
